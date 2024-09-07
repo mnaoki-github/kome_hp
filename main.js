b50d9c863f3a62eabb5051b5972d4107123c58d9
naoki <n_mgem6_17@yahoo.co.jp>
2024-09-07 17:12:54 +0900

    add blog5

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 'use strick';
 {
-
+// コメの年齢月
 function getKomeAge(){
 
 const age = document.querySelector('#age');
 const birthday= new Date(document.querySelector('#birthday').textContent);
 const today =new Date();
-
 const y = today.getFullYear() - birthday.getFullYear()
 let m = today.getMonth() - birthday.getMonth()
 
 if (today.getDate() < birthday.getDate()) {
     m--;
 }
-
-console.log(today.getFullYear());
-console.log(birthday.getFullYear());
 return `${y}歳${m}カ月`;
 }
-
 age.textContent = getKomeAge();
-
 }
+
+
