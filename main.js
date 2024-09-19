42aaacf91c1441579ce461cb5f24e466af8fb658
naoki <n_mgem6_17@yahoo.co.jp>
2024-09-19 13:28:40 +0900

    auto list

--- main.js
+++ main.js
@@ -14,8 +14,10 @@
     // メインフォルダ名
     const rootFolderName = domainName + '/kome_hp';
     // const rootFolderName = 'http://127.0.0.1:5500';
+
     // headタグ
     const head = document.querySelector('head');
+
     //  favicon設定
     function makeFaviconElement() {
         const faviconFileLink = '/img/favicon.ico';
@@ -111,33 +113,32 @@
 
     };
 
-
     function makeFooterNav() {
-        const footerNav =document.querySelector('#footer-nav')
-        const navList =[
-            ['お知らせ','/news.html'],
-            ['プライパシーポリシー','/policy.html'],
+        const footerNav = document.querySelector('#footer-nav')
+        const navList = [
+            ['お知らせ', '/news.html'],
+            ['プライパシーポリシー', '/policy.html'],
         ];
 
-            for (i=0;i<navList.length;i++) {
-                const navItem =document.createElement('li');
-                const navLink =document.createElement('a');
-                navLink.href=rootFolderName + navList[i][1];
-                navLink.textContent=navList[i][0];
+        for (i = 0; i < navList.length; i++) {
+            const navItem = document.createElement('li');
+            const navLink = document.createElement('a');
+            navLink.href = rootFolderName + navList[i][1];
+            navLink.textContent = navList[i][0];
 
-                navItem.appendChild(navLink);
-                footerNav.appendChild(navItem);
-            }
+            navItem.appendChild(navLink);
+            footerNav.appendChild(navItem);
+        }
     };
 
     function makeSticky() {
-        const sticky =document.querySelector('#sticky');
-        const stickyInsta =document.createElement('a');
-        const instaLogo =document.createElement('i');
+        const sticky = document.querySelector('#sticky');
+        const stickyInsta = document.createElement('a');
+        const instaLogo = document.createElement('i');
         instaLogo.classList.add('fa-brands', 'fa-instagram');
 
-        stickyInsta.href ='https://www.instagram.com/stay_with_kome?igsh=OGE0cjl3ODZ0YnVz';
-        stickyInsta.target='_blank'
+        stickyInsta.href = 'https://www.instagram.com/stay_with_kome?igsh=OGE0cjl3ODZ0YnVz';
+        stickyInsta.target = '_blank'
 
         stickyInsta.appendChild(instaLogo);
         sticky.appendChild(stickyInsta);
@@ -151,11 +152,12 @@
     // コメの年齢
     function getKomeAge() {
 
-        if (window.location.pathname!=='/index.html') {
-            return;
-
-        }
         const age = document.querySelector('#age');
+
+        if (age === null) {
+            return;
+        }
+
         const birthday = new Date(document.querySelector('#birthday').textContent);
         const today = new Date();
         const y = today.getFullYear() - birthday.getFullYear()
@@ -164,9 +166,109 @@
         if (today.getDate() < birthday.getDate()) {
             m--;
         }
-        return `${y}歳${m}カ月`;
+        age.textContent = `${y}歳${m}カ月`;
     }
-    age.textContent = getKomeAge();
+
+    getKomeAge();
+
+    // list initialize
+    let listData = [];
+    let blogList = [];
+    let cnt = 0;
+    let cntBlog = 0;
+
+    async function getNewsData() {
+
+        const requestURL = rootFolderName + '/newslist.json';
+        const request = new Request(requestURL);
+        const response = await fetch(request);
+        const data = await response.json();
+
+        listData = data;
+        blogList = data.filter((prop) => {
+            if (prop.type === 'blog') {
+                return prop;
+            };
+        });
+
+        // 初期リスト作成
+        mkList(cnt, listData,'#news-list');
+        mkList(cntBlog, blogList,'#blog-list');
+    };
+
+    function mkList(j, data,idName) {
+        const ul = document.querySelector(idName);
+
+        if (ul === null) {
+            return;
+        }
+
+        for (let i = j; i < j + 5; i++) {
+            if (i === data.length-1) {
+                const clickBtn=document.querySelector('.btn');
+                clickBtn.classList.add('hide');
+            }
+            if (i === data.length) {
+                return;
+            }
+            const li = document.createElement('li');
+            li.classList.add('newsItem');
+
+            const div = document.createElement('div');
+            const spanDate = document.createElement('span');
+            const spanCategory = document.createElement('span');
+
+            spanDate.textContent = data[i].date;
+            spanDate.classList.add('date');
+
+            spanCategory.textContent = data[i].type;
+            let title = document.createElement('a');
+
+            if (data[i].type === 'blog') {
+                spanCategory.classList.add('category', 'blogItem');
+                title.href = rootFolderName + '/blog/' + data[i].file;
+            } else {
+                spanCategory.classList.add('category');
+                title = document.createElement('span');
+            }
+            title.textContent = data[i].title;
+
+            div.appendChild(spanDate);
+            div.appendChild(spanCategory);
+            li.appendChild(div);
+            li.appendChild(title);
+
+            ul.appendChild(li);
+        };
+        cnt += 5;
+        cntBlog +=5;
+    }
+
+    // newsのbtnクリックで表示増やす
+    function listAdd() {
+        const openBtn = document.querySelector('#news-open-btn');
+        if (openBtn === null) {
+            return;
+        }
+
+        openBtn.addEventListener('click', () => {
+            mkList(cnt, listData,'#news-list');
+        })
+    };
+
+    // blogのbtnクリックで表示増やす
+    function blogListAdd() {
+        const openBtn = document.querySelector('#blog-open-btn');
+        if (openBtn === null) {
+            return;
+        }
+        openBtn.addEventListener('click', () => {
+            mkList(cntBlog, blogList,'#blog-list');
+        })
+    };
+
+    getNewsData();
+    listAdd();
+    blogListAdd();
+
 }
-
-
